ceeca8e7451affd71019345cfef608e8c89601ce
amancd <imamanwp@gmail.com>
2024-02-11 20:10:23 +0530

    Added the code

--- Basic Maths/divisors.py
+++ Basic Maths/divisors.py
@@ -1,5 +1,17 @@
 # num = 36
 
 # 36 times iteration that is not good
-# Optimal Solution
 
+# Optimal Solution
+# a * a = n
+# a = sqrt n
+# a * b < = n
+# b = n//a
+
+num = 36
+
+for i in range(1, int(num**0.5)+1):
+    if num%i==0:
+        print(i)
+        if (i!=num//i):
+            print(num//i)
