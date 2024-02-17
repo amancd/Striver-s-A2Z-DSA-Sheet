803fd213a684490e376c291f51eab2b1b74deaf0
amancd <imamanwp@gmail.com>
2024-02-17 13:58:30 +0530

    Updated

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -6,7 +6,6 @@ def bubblesort(arr):
         for j in range(0, i):
             if arr[j]>=arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-            n = n - 1
     
     print(arr)
 
