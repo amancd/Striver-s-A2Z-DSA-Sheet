5253b9c4fd9089b1899a8a20db9cb96bc51c442c
amancd <imamanwp@gmail.com>
2024-02-26 23:30:17 +0530

    First commit

--- Arrays/Easy/longestsubarray.py
+++ Arrays/Easy/longestsubarray.py
@@ -1,13 +1,9 @@
 # Longest subarray with given sum k(positives)
-
 # N = 3, k = 5, array[] = {2, 3, 5}
 # 2
 
-# return largest subarray length
-
 def fn(arr, k):
     count = 0
-
     for i in range(len(arr)):
        sum = 0
        for j in range(i, len(arr)):
